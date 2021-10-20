6400e12c66fd83a03505043f5dc9254dd896ff40
Mounica Kamireddy <kamireddym28@gmail.com>
2021-10-19 23:06:49 -0700

    point to beanstalk backendURL

--- react-view/src/components/utils.js
+++ react-view/src/components/utils.js
@@ -1,4 +1,5 @@
-let urls = {
-    backendURL : "http://localhost:8081"
+const urls = {
+    // backendURL : "http://localhost:8080"
+    backendURL: "http://cloud-vault-dev.us-east-2.elasticbeanstalk.com"
 }
 export default urls

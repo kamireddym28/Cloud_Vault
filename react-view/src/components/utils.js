bf53f98f4efb5a498188f74c4209988af31abd40
Mounica Kamireddy <kamireddym28@gmail.com>
2021-10-20 15:56:23 -0700

    revert utils

--- react-view/src/components/utils.js
+++ react-view/src/components/utils.js
@@ -1,5 +1,5 @@
 const urls = {
-    backendURL : "http://localhost:8080"
-    //backendURL: "http://cloud-vault-dev.us-east-2.elasticbeanstalk.com"
+    // backendURL : "http://localhost:8080"
+    backendURL: "http://cloud-vault-dev.us-east-2.elasticbeanstalk.com"
 }
 export default urls
